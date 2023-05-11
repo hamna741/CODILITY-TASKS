c530b6a1a16f985b69a7e237ac612a2069c32e48
hamna noor <hamna@rakurai.io>
2023-05-11 10:34:05 +0500

    fixed removed extra lines of code

--- src/lesson6/intersection.cpp
+++ src/lesson6/intersection.cpp
@@ -5,19 +5,19 @@
 #include <algorithm>
 
 int cars(std::vector<int> &A){
- int n = A.size();
-    std::vector<int> left(n), right(n);
-    for (int i = 0; i < n; i++) {
+ 
+    std::vector<int> left(A.size()), right(A.size());
+    for (int i = 0; i < A.size(); i++) {
         left[i] = i - A[i];
         right[i] = i + A[i];
     }
     std::sort(left.begin(), left.end());
     std::sort(right.begin(), right.end());
-    int intersections = 0, j = 0;
-    for (int i = 0; i < n; i++) {
-        while (j < n && right[i] >= left[j]) {
-            intersections += j - i;
-            j++;
+    int intersections = 0, circle2 = 0;
+    for (int circle1 = 0; circle1 < A.size(); circle1++) {
+        while (circle2 < A.size() && right[circle1] >= left[circle2]) {
+            intersections += circle2- circle1;
+            circle2++;
             if (intersections > 10000000) {
                 return -1;
             }
