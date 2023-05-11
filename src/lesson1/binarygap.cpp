b9f93fc7506409ec19058b54b23e4bc735eefe8b
hamna noor <hamna@rakurai.io>
2023-05-11 21:37:26 +0500

    modified version of solution function

--- src/lesson1/binarygap.cpp
+++ src/lesson1/binarygap.cpp
@@ -1,21 +1,20 @@
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
 #include<iostream>
 #include <bitset>
-#include <cstdlib>
-int main(int argc, char* argv[]){
-    if(argc!=2){
-        std::cout<<"enter number!!";
-        return 1;
-    }
-    else{
 
-   
-    std::bitset<32> binary(atoi(argv[1])); //binary representation
+int solution(int N) {
+std::bitset<32> binary(N); //binary representation
     int max_zeros = 0;
     int zero_count = 0;
     bool zero_flag = false;
-    std::cout<<"BINARY OF "<<atoi(argv[1])<<" " <<binary<<std::endl;
+   
     for (int i = 0; i < 32; i++) {
-        if (binary.test(i)) {  //'1'
+        if (binary[i]) {  //'1'
             if (zero_flag) { // a gap
                 max_zeros = std::max(max_zeros, zero_count);
                 zero_count = 0; // reset the counter
@@ -28,7 +27,5 @@ int main(int argc, char* argv[]){
             }
         }
     }
-    std::cout<<"NUMBER OF CONSECTIVE ZEROS: "<<max_zeros<<std::endl;
-    return 0;
-}
+      return max_zeros;
 }
