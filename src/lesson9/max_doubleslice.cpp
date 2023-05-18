748a9bf6d2613deb3cc751b92a173b6e0c5b396d
hamna noor <hamna@rakurai.io>
2023-05-18 16:12:18 +0500

    formatted code

--- src/lesson9/max_doubleslice.cpp
+++ src/lesson9/max_doubleslice.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 // you can use includes, for example:
 // #include <algorithm>
@@ -7,27 +7,32 @@
 // you can write to stdout for debugging purposes, e.g.
 // cout << "this is a debug message" << endl;
 
-int solution(std::vector<int> &A) {
-   
-std::vector<int> K1(A.size(),0);
-std::vector<int> K2(A.size(),0);
+int solution(std::vector<int> &A)
+{
 
-  for(unsigned int i = 1; i < A.size()-1; i++){
-    K1[i] = std::max(K1[i-1] + A[i], 0);
+  std::vector<int> K1(A.size(), 0);
+  std::vector<int> K2(A.size(), 0);
+
+  for (unsigned int i = 1; i < A.size() - 1; i++)
+  {
+    K1[i] = std::max(K1[i - 1] + A[i], 0);
   }
-  for(unsigned int i = A.size()-2; i > 0; i--){
-    K2[i] = std::max(K2[i+1]+A[i], 0);
+  for (unsigned int i = A.size() - 2; i > 0; i--)
+  {
+    K2[i] = std::max(K2[i + 1] + A[i], 0);
   }
 
   int max = 0;
 
-  for(unsigned int i = 1; i < A.size()-1; i++){
-    max = std::max(max, K1[i-1]+K2[i+1]);
+  for (unsigned int i = 1; i < A.size() - 1; i++)
+  {
+    max = std::max(max, K1[i - 1] + K2[i + 1]);
   }
 
   return max;
 }
-int main(){
-    std::vector<int>A={3,2,6,-1,4,5,-1,2};
-    std::cout<<solution(A)<<std::endl;
+int main()
+{
+  std::vector<int> A = {3, 2, 6, -1, 4, 5, -1, 2};
+  std::cout << solution(A) << std::endl;
 }
