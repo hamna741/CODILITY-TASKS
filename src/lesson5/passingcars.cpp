453f943156dd7785051fa3d683c5aa0f7623cbf3
hamna noor <hamna@rakurai.io>
2023-05-18 16:12:00 +0500

    formatted code

--- src/lesson5/passingcars.cpp
+++ src/lesson5/passingcars.cpp
@@ -1,20 +1,27 @@
-#include<vector>
-#include<iostream>
-int solution(std::vector<int> &A) {
+#include <vector>
+#include <iostream>
+int solution(std::vector<int> &A)
+{
     int east = 0, pairs = 0;
-    for (int i = 0; i < A.size(); i++) {
-        if (A[i] == 0) {
+    for (int i = 0; i < A.size(); i++)
+    {
+        if (A[i] == 0)
+        {
             east++;
-        } else {
+        }
+        else
+        {
             pairs += east;
-            if (pairs > 1000000000) {
+            if (pairs > 1000000000)
+            {
                 return -1;
             }
         }
     }
     return pairs;
 }
-int main(){
-    std::vector<int>A={0,1,0,1,1};
-    std::cout<<solution(A);
+int main()
+{
+    std::vector<int> A = {0, 1, 0, 1, 1};
+    std::cout << solution(A);
 }
