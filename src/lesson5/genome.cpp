72f3643d18c4e820774b7a064fa2e6ca85101e89
hamna noor <hamna@rakurai.io>
2023-05-18 16:47:06 +0500

    replaced 2D vector with vector for each nucleotide

--- src/lesson5/genome.cpp
+++ src/lesson5/genome.cpp
@@ -6,66 +6,43 @@
 #include <vector>
 #include <string>
 #include <iostream>
-int getImpact(char nucleotide)
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <bits/stdc++.h>
+std::vector<int> solution(std::string s, std::vector<int> &p, std::vector<int> &Q)
 {
-    switch (nucleotide)
+    std::vector<int> result;
+    int N = s.size(), M = p.size();
+    std::vector<int> countA(N, 0);
+    std::vector<int> countC(N, 0);
+    std::vector<int> countG(N, 0);
+    std::vector<int> countT(N, 0);
+    for (int index = 1; index < N; index++)
     {
-    case 'A':
-        return 0;
-    case 'C':
-        return 1;
-    case 'G':
-        return 2;
-    case 'T':
-        return 3;
-    default:
-        return -1;
+        countA[index]=countA[index-1]+(s[index-1]=='A');
+        countC[index]=countC[index-1]+(s[index-1]=='C');
+        countG[index]=countG[index-1]+(s[index-1]=='G');
+        countT[index]=countT[index-1]+(s[index-1]=='T');
     }
-}
-std::vector<int> solution(std::string &S, std::vector<int> &P, std::vector<int> &Q)
-{
-    //  int N = S.size();
-    std::vector<int> result(P.size());
-    std::vector<std::vector<int>> prefix_sum(S.size(), std::vector<int>(4, 0));
-    prefix_sum[0][getImpact(S[0])]++;
-
-    for (int i = 1; i < S.size(); i++)
-    { // for each P-Q range
-        prefix_sum[i][0] = prefix_sum[i - 1][0];
-        prefix_sum[i][1] = prefix_sum[i - 1][1];
-        prefix_sum[i][2] = prefix_sum[i - 1][2];
-        prefix_sum[i][3] = prefix_sum[i - 1][3];
-        prefix_sum[i][getImpact(S[i])]++;
-    }
-    for (const auto &row : prefix_sum)
+    int numberofA=0, numberofC=0, numberofG=0, numberofT=0;
+    for (int i = 0; i < M; i++)
     {
-        for (int element : row)
-        {
-            std::cout << element << " ";
-        }
-        std::cout << std::endl;
-    }
-
-    for (unsigned int i = 0; i < P.size(); i++) // p={2, 5, 0}; q={4, 5, 6};
-    {
-
-        int count = 0;
-        for (int j = 0; j < 4; j++)
-        {
-
-            if (P[i] > 0)
-            {
-                count = prefix_sum[Q[i]][j] - prefix_sum[P[i] - 1][j];
-            }
-            else
-                count = prefix_sum[Q[i]][j];
-
-            if (count > 0)
-            {
-                result[i] = j + 1;
-                break;
-            }
-        }
+       
+        numberofA=countA[Q[i]+1]-countA[p[i]];
+        numberofC=countC[Q[i]+1]-countC[p[i]];
+        numberofG=countG[Q[i]+1]-countG[p[i]];
+        numberofT=countT[Q[i]+1]-countT[p[i]];
+        
+        if(numberofA>0)
+        result.push_back(1);
+        else if(numberofC>0)
+        result.push_back(2);
+        else if (numberofG>0)
+        result.push_back(3);
+        else
+        result.push_back(4);
     }
 
     return result;
@@ -77,5 +54,6 @@ int main()
     std::vector<int> Q = {5, 5, 6};
 
     for (auto val : solution(s, P, Q))
-        std::cout << val;
+        std::cout << val<<"\t";
+        std::cout<<std::endl;
 }
